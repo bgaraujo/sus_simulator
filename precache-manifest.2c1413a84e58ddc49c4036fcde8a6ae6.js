840583d457dafea474b74db0d3e3f3727df2d479
Bruno <brunogomes@live.it>
2019-12-13 18:24:52 -0300

    Updates

---
+++ precache-manifest.2c1413a84e58ddc49c4036fcde8a6ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fa8d7f72c5f885fd66cecd15aafb12",
+    "url": "/sus_simulator/index.html"
+  },
+  {
+    "revision": "d040f2488f8a717e94a4",
+    "url": "/sus_simulator/static/css/main.256f0245.chunk.css"
+  },
+  {
+    "revision": "58b549b54d9a40de0826",
+    "url": "/sus_simulator/static/js/2.1cc8e975.chunk.js"
+  },
+  {
+    "revision": "d040f2488f8a717e94a4",
+    "url": "/sus_simulator/static/js/main.6bf39069.chunk.js"
+  },
+  {
+    "revision": "6843a032b7d803714b46",
+    "url": "/sus_simulator/static/js/runtime-main.a8aeb032.js"
+  },
+  {
+    "revision": "0ac2a766ea172a5a4747eff91ef4597a",
+    "url": "/sus_simulator/static/media/susIco.0ac2a766.png"
+  }
+]);
